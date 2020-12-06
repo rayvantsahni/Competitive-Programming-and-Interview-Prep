f85bc8003e7d033a3cb312549da32b3199a4ce2b
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-12-07 00:54:33 +0530

    Add files via upload

---
+++ CodeChef/vaccine_production.py
@@ -0,0 +1,13 @@
+d1, v1, d2, v2, p = map(int, input().split())
+
+day = 0
+total_vaccines = 0
+
+while total_vaccines < p:
+    if d1 <= day:
+        total_vaccines += v1
+    if d2 <= day:
+        total_vaccines += v2
+    day += 1
+
+print(day - 1)
