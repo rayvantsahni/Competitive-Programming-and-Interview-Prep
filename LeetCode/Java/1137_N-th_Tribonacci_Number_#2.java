2e50e19a407d255fdbad7ae2bf7a118b033f1a60
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2021-01-22 18:20:21 +0530

    Create 1137_N-th_Tribonacci_Number_#2.java

---
+++ LeetCode/Java/1137_N-th_Tribonacci_Number_#2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int tribonacci(int n) {
+        if (n == 0 || n == 1)
+            return n;
+        if (n == 2)
+            return 1;
+
+        int[] tribo = new int[n + 1];
+
+        tribo[0] = 0;
+        tribo[1] = 1;
+        tribo[2] = 1;
+
+        for (int i = 3; i <= n; i++)
+            tribo[i] = tribo[i - 1] + tribo[i - 2] + tribo[i - 3];
+
+        return tribo[n];
+    }
+}
