ca8fbb85274e05fb2ba4d96ed9e55ad7ec635a62
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2021-01-29 13:28:19 +0530

    Delete 0242_Valid_Anagram_SecondApproach.py

--- LeetCode/0242_Valid_Anagram_SecondApproach.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if not len(s) and not len(t):
-            return True
-        if len(s) != len(t):
-            return False
-        
-        return sorted(s) == sorted(t)
