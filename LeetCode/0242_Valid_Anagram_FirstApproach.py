0857d0dbf3cfd5b91bd0fde6c34851e9b4590b24
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2021-01-29 13:27:28 +0530

    Delete 0242_Valid_Anagram_FirstApproach.py

--- LeetCode/0242_Valid_Anagram_FirstApproach.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if not len(s) and not len(t):
-            return True
-        if len(s) != len(t):
-            return False
-        
-        from collections import Counter
-        return Counter(s) == Counter(t)
