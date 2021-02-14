f7f74d5bc059c06a03822f2c2382c019a063538a
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2021-02-15 01:46:57 +0530

    Create Java Hashset.java

---
+++ HackerRank/Java/Java Hashset.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+ public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+//Write your code here
+    HashSet<String> set = new HashSet<>();
+    for (int i = 0; i < pair_left.length; i++) {
+        String name = pair_left[i] + " " + pair_right[i];
+        set.add(name);
+        System.out.println(set.size());
+    }
+   }
+}
