d6700ffac561dc121b9eebe3cad3543cc7d1f56a
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2021-02-14 22:29:27 +0530

    Create highest_divisor.py

---
+++ CodeChef/highest_divisor.py
@@ -0,0 +1,9 @@
+# Problem Code:HDIVISR
+
+def highest_divisor(n):
+    for i in range(10, 0, -1):
+        if not n % i:
+            return i
+            
+n = int(input())
+print(highest_divisor(n))
