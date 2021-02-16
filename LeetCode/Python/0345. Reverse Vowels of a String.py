823ccce36a4ce73e6838c532ce915c55b8e453ee
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2021-02-17 01:49:01 +0530

    Create 0345. Reverse Vowels of a String.py

---
+++ LeetCode/Python/0345. Reverse Vowels of a String.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        s = list(s)
+        left, right = 0, len(s) - 1
+        
+        while left < right:
+            if not self.is_vowel(s[left]):
+                left += 1
+                continue
+
+            if not self.is_vowel(s[right]):
+                right -= 1
+                continue
+
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+        return "".join(s)
+        
+    def is_vowel(self, letter):
+        return ord(letter) in (65, 69, 73, 79, 85, 97, 101, 105, 111, 117)
