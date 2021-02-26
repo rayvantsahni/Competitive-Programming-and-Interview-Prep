6d79ca601499f88281184160ff052dd038746645
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2021-02-26 21:36:25 +0530

    Create 1486. XOR Operation in an Array.py

---
+++ LeetCode/Python/1486. XOR Operation in an Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        xor = 0
+        
+        for i in range(n):
+            xor ^= start + (2 * i)
+        
+        return xor
