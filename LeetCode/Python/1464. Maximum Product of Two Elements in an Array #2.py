af567d0a8f1eaa735a7a7cc984b61a935737e58e
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2021-02-28 03:24:49 +0530

    Update 1464. Maximum Product of Two Elements in an Array #2.py

--- LeetCode/Python/1464. Maximum Product of Two Elements in an Array #2.py
+++ LeetCode/Python/1464. Maximum Product of Two Elements in an Array #2.py
@@ -3,12 +3,9 @@ class Solution:
         first = second = -math.inf
         
         for num in nums:
-            if num == 1:
-                continue
             if num > first:
                 first, second = num, first
             elif num > second:
                 second = num
 
-        product = (first - 1) * (second - 1)
-        return product if not product == inf else 0
+        return (first - 1) * (second - 1)
