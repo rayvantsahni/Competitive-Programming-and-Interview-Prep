7aa2d58ac1f92ccf6606af65d09323f3bd5ed1a5
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2021-03-03 01:58:33 +0530

    Create 0136. Single Number.java

---
+++ LeetCode/Java/0136. Single Number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        
+        for (int num: nums)
+            xor ^= num;
+        
+        return xor;
+    }
+}
