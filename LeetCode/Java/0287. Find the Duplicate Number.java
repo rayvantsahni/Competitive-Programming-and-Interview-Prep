f4c191e490caceef73ac2baab279200941a34ab4
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2021-03-03 02:12:59 +0530

    Create 0287. Find the Duplicate Number.java

---
+++ LeetCode/Java/0287. Find the Duplicate Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int num: nums) {
+            if (set.contains(num))
+                return num;
+            set.add(num);
+        }
+        
+        return -1;
+    }
+}
