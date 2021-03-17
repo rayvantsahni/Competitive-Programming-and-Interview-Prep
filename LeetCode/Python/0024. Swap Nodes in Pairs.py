ca928efd7680cfab808d8981329441915aaa6dd7
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2021-03-18 00:43:29 +0530

    Create 0024. Swap Nodes in Pairs.py

---
+++ LeetCode/Python/0024. Swap Nodes in Pairs.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        current = head
+        
+        while current and current.next:
+            current.val, current.next.val = current.next.val, current.val
+            current = current.next.next
+        
+        return head
