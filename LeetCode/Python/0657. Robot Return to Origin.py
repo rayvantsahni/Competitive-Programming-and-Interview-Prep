8d2ff5f5bfb0e623000ef71b0818d3374dd8b264
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2021-04-23 21:54:07 +0530

    Update 0657. Robot Return to Origin.py

--- LeetCode/Python/0657. Robot Return to Origin.py
+++ LeetCode/Python/0657. Robot Return to Origin.py
@@ -3,11 +3,11 @@ class Solution:
         x_axis = y_axis = 0
         for move in moves:
             if move == "U":
-                x_axis += 1
-            elif move == "D":
-                x_axis -= 1
-            elif move == "R":
                 y_axis += 1
-            elif move == "L":
+            elif move == "D":
                 y_axis -= 1
+            elif move == "R":
+                x_axis += 1
+            elif move == "L":
+                x_axis -= 1
         return not x_axis and not y_axis
