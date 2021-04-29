b8525fa9bca2d775fa6b918bc2807adcbbd05a45
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2021-04-29 13:01:10 +0530

    Update Finding middle element in a linked list.py

--- GeeksforGeeks/Finding middle element in a linked list.py
+++ GeeksforGeeks/Finding middle element in a linked list.py
@@ -1,3 +1,12 @@
+# your task is to complete this function
+
+'''
+class node:
+    def __init__(data):
+        self.data = data
+        self.next = None
+'''
+
 # function should return index to the any valid peak element
 def findMid(head):
     # Code here
@@ -9,3 +18,46 @@ def findMid(head):
         fast = fast.next.next
         
     return slow.data
+
+
+#{ 
+#  Driver Code Starts
+# Node Class    
+class node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+        
+# Linked List Class
+class Linked_List:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert(self, val):
+        if self.head == None:
+            self.head = node(val)
+            self.tail = self.head
+        else:
+            new_node = node(val)
+            self.tail.next = new_node
+            self.tail = self.tail.next
+
+def createList(arr, n):
+    lis = Linked_List()
+    for i in range(n):
+        lis.insert(arr[i])
+    return lis.head
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        head = createList(arr, n)
+        print(findMid(head))
+
+
+
+
+# } Driver Code Ends
