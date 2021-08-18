3127b4c5280ee2dbd2036cc1f4b9b993466265bf
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2021-08-19 03:53:53 +0530

    Create 0797. All Paths From Source to Target.py

---
+++ LeetCode/Python/0797. All Paths From Source to Target.py
@@ -0,0 +1,13 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        all_paths = []
+        self._allPathsSourceTarget(graph, 0, [0], all_paths)
+        return all_paths
+    
+    def _allPathsSourceTarget(self, graph, source, current, all_paths):
+        if source == len(graph) - 1:
+            all_paths.append(current)
+        
+        neighbors = graph[source]
+        for neighbor in neighbors:
+            self._allPathsSourceTarget(graph, neighbor, current + [neighbor], all_paths)
