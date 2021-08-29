9aedc73aeeb083a276af177588cfa31e4dacf38e
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2021-08-29 22:40:01 +0530

    Add files via upload

---
+++ CSES/coin_piles.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+def input():
+    return stdin.readline()
+
+def coin_piles(a, b):
+    return not ((a + b) % 3) and max(a, b) <= 2 * min(a, b)
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        a, b = map(int, input().split())
+        print("YES" if coin_piles(a, b) else "NO")
